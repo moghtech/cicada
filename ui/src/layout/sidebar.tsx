f121f4ce341618530417e5cd51bebe003ea45350
mbecker20 <becker.maxh@gmail.com>
2026-01-04 12:20:16 -0800

    sidebar filesystem / file tree

--- ui/src/layout/sidebar.tsx
+++ ui/src/layout/sidebar.tsx
@@ -1,61 +1,61 @@
 import { useRead } from "@/lib/hooks";
 import {
+  ActionIcon,
   Button,
   Center,
+  Divider,
   Flex,
   Group,
   Loader,
+  ScrollArea,
+  Text,
   Tree,
   TreeNodeData,
 } from "@mantine/core";
-import { ChevronRight, File } from "lucide-react";
-import { useNavigate } from "react-router-dom";
+import { ChevronRight, File, FolderOpen, HardDrive, Link2 } from "lucide-react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 export const Sidebar = () => {
-  const { data: filesystems } = useRead("ListFilesystems", {});
+  const { filesystem: selected_filesystem, inode: _selected_inode } =
+    useParams() as {
+      filesystem?: string;
+      inode?: string;
+    };
 
-  if (!filesystems) {
-    return (
-      <Center h="100%">
-        <Loader size="lg" />
-      </Center>
-    );
-  }
-
-  const data: TreeNodeData[] = filesystems.map((fs) => ({
-    value: fs.id,
-    label: fs.name,
-    children: [
-      { value: "children", label: <NodeTree filesystem={fs.id} parent={1} /> },
-    ],
-  }));
+  const n_selected_inode = _selected_inode
+    ? Number(_selected_inode)
+    : undefined;
+  const selected_inode = n_selected_inode ? n_selected_inode : undefined;
 
   return (
-    <Flex direction="column" m={16}>
-      <Filesystems />
-      <Tree
-        data={data}
-        renderNode={({ node, expanded, hasChildren, elementProps }) => (
-          <Group gap={5} {...elementProps}>
-            {hasChildren && (
-              <ChevronRight
-                size={18}
-                style={{
-                  transitionProperty: "transform",
-                  transform: expanded ? "rotate(90deg)" : "rotate(0deg)",
-                }}
-              />
-            )}
+    <ScrollArea>
+      <Flex direction="column" gap="lg" m={16}>
+        <Filesystems filesystem={selected_filesystem} />
+        <Divider />
+        {selected_filesystem ? (
+          <Flex direction="column" gap="sm">
+            <Flex gap="sm" opacity={0.7} align="center">
+              <FolderOpen size="1rem" />
+              <Text>Files</Text>
+            </Flex>
 
-            <span>{node.label}</span>
-          </Group>
+            <NodeTree
+              filesystem={selected_filesystem}
+              parent={1}
+              selected={selected_inode}
+            />
+          </Flex>
+        ) : (
+          <Center>
+            <Text>No Filesystem Selected</Text>
+          </Center>
         )}
-      />
-    </Flex>
+      </Flex>
+    </ScrollArea>
   );
 };
 
-const Filesystems = () => {
+const Filesystems = ({ filesystem }: { filesystem: string | undefined }) => {
   const { data: filesystems } = useRead("ListFilesystems", {});
 
   if (!filesystems) {
@@ -67,13 +67,24 @@ const Filesystems = () => {
   }
 
   return (
-    <>
+    <Flex direction="column" gap="sm">
+      <Flex gap="sm" opacity={0.7} align="center">
+        <HardDrive size="1rem" />
+        <Text>Filesystems</Text>
+      </Flex>
       {filesystems.map((fs) => (
-        <Button key={fs.id} variant="subtle" color="inherit">
+        <Button
+          key={fs.id}
+          variant={fs.id === filesystem ? "default" : "subtle"}
+          color="inherit"
+          justify="start"
+          component={Link}
+          to={`/filesystems/${fs.id}`}
+        >
           {fs.name}
         </Button>
       ))}
-    </>
+    </Flex>
   );
 };
 
@@ -83,9 +94,11 @@ const CHILD_VALUE_IDENTIFIER = "__CHILD__";
 const NodeTree = ({
   filesystem,
   parent,
+  selected,
 }: {
   filesystem: string;
   parent: number;
+  selected: number | undefined;
 }) => {
   const nav = useNavigate();
   const nodes = useRead("ListNodes", { filesystem, parent }).data ?? [];
@@ -99,7 +112,11 @@ const NodeTree = ({
             {
               value: CHILD_VALUE_IDENTIFIER,
               label: (
-                <NodeTree filesystem={node.filesystem} parent={node.inode} />
+                <NodeTree
+                  filesystem={node.filesystem}
+                  parent={node.inode}
+                  selected={selected}
+                />
               ),
             },
           ]
@@ -108,35 +125,65 @@ const NodeTree = ({
 
   return (
     <Tree
+      w="100%"
       data={data}
-      renderNode={({ node, expanded, hasChildren, elementProps }) => (
-        <Group
-          gap={5}
-          {...elementProps}
-          onClick={(e) =>
-            node.value === CHILD_VALUE_IDENTIFIER
-              ? undefined
-              : hasChildren
+      renderNode={({ node, expanded, hasChildren, elementProps }) => {
+        if (node.value === CHILD_VALUE_IDENTIFIER) {
+          return <Flex {...elementProps}>{node.label}</Flex>;
+        }
+
+        const inode = Number(node.value.split("/")[1]);
+
+        return (
+          <Button
+            variant={selected && selected === inode ? "default" : "subtle"}
+            color="inherit"
+            p="0rem 0.5rem"
+            mb="0.25rem"
+            justify="space-between"
+            fullWidth
+            rightSection={
+              hasChildren && (
+                <ActionIcon
+                  component="div"
+                  variant="subtle"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    nav("/filesystems/" + node.value);
+                  }}
+                >
+                  <Link2 size="1rem" />
+                </ActionIcon>
+              )
+            }
+            {...elementProps}
+            onClick={(e) =>
+              hasChildren
                 ? elementProps.onClick(e)
                 : nav("/filesystems/" + node.value)
-          }
-        >
-          {node.value !== CHILD_VALUE_IDENTIFIER &&
-            (hasChildren ? (
+            }
+          >
+            {hasChildren ? (
               <ChevronRight
-                size={18}
+                size="1rem"
                 style={{
                   transitionProperty: "transform",
                   transform: expanded ? "rotate(90deg)" : "rotate(0deg)",
+                  marginRight: "0.5rem",
                 }}
               />
             ) : (
-              <File size={18} />
-            ))}
-
-          <span>{node.label}</span>
-        </Group>
-      )}
+              <File
+                size="1rem"
+                style={{
+                  marginRight: "0.5rem",
+                }}
+              />
+            )}
+            {node.label}
+          </Button>
+        );
+      }}
     />
   );
 };
