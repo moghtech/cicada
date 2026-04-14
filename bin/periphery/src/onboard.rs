6206bb3f6192ae6c73cfde8090fd436c74cbf349
mbecker20 <becker.maxh@gmail.com>
2026-04-14 01:52:32 -0700

    add status logs to onboarding flow

--- bin/periphery/src/onboard.rs
+++ bin/periphery/src/onboard.rs
@@ -14,34 +14,38 @@ use crate::{
 /// Make sure auth is valid, if not then try onboarding device.
 pub async fn ensure_onboarded() -> anyhow::Result<()> {
   tokio::task::spawn_blocking(|| {
-      if cicada().read(GetVersion{}).is_ok() {
-        return Ok(());
+    if cicada().read(GetVersion{}).is_ok() {
+      info!("Authenticated with Cicada Core");
+      return Ok(());
+    }
+
+    info!("Failed to authenticate with Cicada Core, attempting onboarding...");
+
+    let config = periphery_config();
+    let Some(onboarding_key) = &config.onboarding_key else {
+      return Err(anyhow!("Unable to authenticate with Cicada and no onboarding key is configured."))
+    };
+
+    let onboarding_key = Pkcs8PrivateKey::from_maybe_raw_bytes(onboarding_key)?;
+    let onboarding_client = CicadaClient::new(
+      &config.core_address,
+      ClientType::OnboardingKey,
+      &onboarding_key,
+      core_public_key(),
+    )?;
+
+    onboarding_client.write(
+      CreateDevice {
+        name: config.device_name.clone(),
+        enabled: true,
+        public_key: periphery_keys().load().public.clone().into_inner(),
+        groups: Vec::new()
       }
+    ).context("Failed to create device")?;
 
-      let config = periphery_config();
-      let Some(onboarding_key) = &config.onboarding_key else {
-        return Err(anyhow!("Unable to authenticate with Cicada and no onboarding key is configured."))
-      };
+    info!(device_name = config.device_name, "Device onboarded successfully");
 
-      let onboarding_key = Pkcs8PrivateKey::from_maybe_raw_bytes(onboarding_key)?;
-      let onboarding_client = CicadaClient::new(
-        &config.core_address,
-        ClientType::OnboardingKey,
-        &onboarding_key,
-        core_public_key(),
-      )?;
-
-      onboarding_client.write(
-        CreateDevice {
-          name: config.device_name.clone(),
-          enabled: true,
-          public_key: periphery_keys().load().public.clone().into_inner(),
-          groups: Vec::new()
-        }
-      ).context("Failed to create device")?;
-
-      Ok(())
-    })
-      .await??;
+    Ok(())
+  }).await??;
   Ok(())
 }
