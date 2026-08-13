aaf329f96049cc90c082b40d87e52d6de3bda8fb
mbecker20 <becker.maxh@gmail.com>
2026-08-12 17:01:31 -0700

    run action more simple

--- action/run.ts
+++ action/run.ts
@@ -5,18 +5,7 @@ export const run = async (action: string) => {
     .output()
     .then((r) => new TextDecoder("utf-8").decode(r.stdout).trim());
 
-  // Cargo check first here to make sure lock file is updated before commit.
-  const cmd = `
-cargo check
-
-km run -y action ${action} "CICADA_BRANCH=${branch}"
-`
-    .split("\n")
-    .map((line) => line.trim())
-    .filter((line) => line.length > 0 && !line.startsWith("//"))
-    .join(" && ");
-
   new Deno.Command("bash", {
-    args: ["-c", cmd],
+    args: ["-c", `km run -y action ${action} "CICADA_BRANCH=${branch}"`],
   }).spawn();
 };
