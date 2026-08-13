ba9fd0ae30e539ff3f9499296f80dc687df6edfe
mbecker20 <becker.maxh@gmail.com>
2026-08-12 17:25:34 -0700

    fix branch passing

--- action/run.ts
+++ action/run.ts
@@ -6,6 +6,6 @@ export const run = async (action: string) => {
     .then((r) => new TextDecoder("utf-8").decode(r.stdout).trim());
 
   new Deno.Command("bash", {
-    args: ["-c", `km run -y action ${action} "CICADA_BRANCH=${branch}"`],
+    args: ["-c", `km run -y action ${action} "BRANCH=${branch}"`],
   }).spawn();
 };
